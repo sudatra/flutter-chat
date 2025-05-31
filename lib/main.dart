954efa528eb119bd459d078df07da6a623eb9b4b
sudatra <mail2sudatra@gmail.com>
2025-05-31 12:31:25 +0530

    added login page setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/pages/login_page.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({ super.key });
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: true,
+      home: LoginPage(),
     );
   }
 }
