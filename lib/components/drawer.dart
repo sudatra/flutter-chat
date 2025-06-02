80bbc8e5d7dbe8c5353b1bc208ba24c1ffc4187d
sudatra <mail2sudatra@gmail.com>
2025-06-02 20:25:33 +0530

    added side drawer

---
+++ lib/components/drawer.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat/auth/auth_service.dart';
+import 'package:flutter_chat/pages/settings_page.dart';
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({super.key});
+
+  void logout() async {
+    final authService = AuthService();
+    authService.signOut();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              DrawerHeader(
+                child: Center(
+                  child: Icon(
+                    Icons.message,
+                    color: Theme.of(context).colorScheme.primary,
+                    size: 40,
+                  )
+                )
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: Text("H O M E"),
+                  leading: Icon(Icons.home),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: Text("S E T T I N G S"),
+                  leading: Icon(Icons.settings),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => SettingsPage()));
+                  },
+                ),
+              )
+            ],
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0, bottom: 25.0),
+            child: ListTile(
+              title: Text("L O G O U T"),
+              leading: Icon(Icons.logout),
+              onTap: logout
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,24 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_chat/auth/auth_service.dart';
+import 'package:flutter_chat/components/drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  void logout() async {
-    final authService = AuthService();
-    authService.signOut();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        actions: [
-          IconButton(onPressed: logout, icon: Icon(Icons.logout))
-        ],
-      )
+        title: Center(
+          child: Text("Home", textAlign: TextAlign.center)
+        ),
+        backgroundColor: Theme.of(context).colorScheme.primary
+      ),
+      drawer: CustomDrawer(),
     );
   }
 }
