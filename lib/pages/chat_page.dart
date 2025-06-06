3112fcc5766d60397d91a6ae478bdf441938fd73
sudatra <mail2sudatra@gmail.com>
2025-06-06 19:49:07 +0530

    fucntionality completed

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/components/text_field.dart';
 import 'package:flutter_chat/services/auth/auth_service.dart';
 import 'package:flutter_chat/services/chat/chat_service.dart';
 
@@ -37,7 +38,9 @@ class ChatPage extends StatelessWidget {
         children: [
           Expanded(
             child: _buildMessageList(),
-          )
+          ),
+
+          _buildUserInput()
         ],
       ),
     );
@@ -66,7 +69,30 @@ class ChatPage extends StatelessWidget {
 
   Widget _buildMessageItem(DocumentSnapshot doc) {
     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    bool isCurrentUser = (data['senderId'] == _authService.getCurrentUser()!.uid);
+    var alignment = isCurrentUser ? Alignment.centerRight : Alignment.centerLeft;
 
-    return Text(data["message"]);
+    return Container(
+      alignment: alignment,
+      child: Column(
+        crossAxisAlignment: isCurrentUser ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          Text(data["message"]),
+        ],
+      )
+    );
+  }
+
+  Widget _buildUserInput() {
+    return Row(
+      children: [
+        Expanded(child: CustomTextField(
+          controller: _messageController,
+          hintText: 'Type a Message',
+          obscureText: false,
+        )),
+        IconButton(onPressed: sendMessage, icon: Icon(Icons.arrow_upward))
+      ]
+    );
   }
 }
