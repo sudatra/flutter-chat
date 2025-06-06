2585788c627548629be5eb8144879b025e980395
sudatra <mail2sudatra@gmail.com>
2025-06-06 20:38:00 +0530

    added provider and light dark mode toggle

--- lib/components/chat_bubble.dart
+++ lib/components/chat_bubble.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/themes/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class ChatBubble extends StatelessWidget {
   final String message;
@@ -12,16 +14,24 @@ class ChatBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isDarkMode = Provider.of<ThemeProvider>(context, listen: false).isDarkMode;
+
     return Container(
       decoration: BoxDecoration(
-        color: isCurrentUser ? Colors.green : Colors.grey.shade500,
+        color: isCurrentUser 
+          ? (isDarkMode ? Colors.green.shade600 : Colors.green.shade500)
+          : (isDarkMode ? Colors.grey.shade800 : Colors.grey.shade200),
         borderRadius: BorderRadius.circular(12)
       ),
       padding: EdgeInsets.all(16),
       margin: EdgeInsets.symmetric(vertical: 5, horizontal: 25),
       child: Text(
         message,
-        style: TextStyle(color: Colors.white),
+        style: TextStyle(
+          color: isCurrentUser 
+            ? Colors.white
+            : (isDarkMode ? Colors.white : Colors.black)
+        ),
       ),
     );
   }
